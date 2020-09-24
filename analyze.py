82e57f4de0d3ecc3ce233ec9a0b27bc8426f5d22
cc <274065539@qq.com>
2020-09-24 09:30:05 +0800

    2020/9/24

--- analyze.py
+++ analyze.py
@@ -2,6 +2,8 @@ import json
 import time
 import util
 import redis
+import sys
+print(sys.argv)
 redis=redis.Redis(host="127.0.0.1",port=6379,db=2,decode_responses=True)
 # print(redis.keys())
 lis=[]
@@ -15,11 +17,11 @@ for key in redis.keys():
 
 print("sort winPercent: **********************************")
 lis.sort(key=lambda x:x["winPercent"],reverse=True)
-for i in lis[:10]:
+for i in lis[:int(sys.argv[1])]:
     print(i)
 print("sort rate: ****************************************")
 lis.sort(key=lambda x:x["rate"],reverse=True)
-for i in lis[:10]:
+for i in lis[:int(sys.argv[1])]:
     print(i)
 
 
